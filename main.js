9ad688d0d1f4e367ce8e9949bd74bef925795a8b
Ravindra Singh <raveendraa009@gmail.com>
2026-04-03 12:50:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,80 @@
-const task = document.getElementById('task');
-const addButton = document.getElementById('addtask');
-const taskList = document.getElementById('tasklist');
-const form = document.getElementById('to-do-form');
+const input = document.getElementById("taskInput");
+const addBtn = document.getElementById("addTask");
+const taskList = document.getElementById("taskList");
+const filter = document.getElementById("filter")
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+function addTask(){
+    const taskText = input.value.trim();
+    if (taskText === "") return;
 
-  const taskText = task.value.trim();
-  if (taskText === "") return;
+    const li = document.createElement("li");
 
-  const li = document.createElement("li");
+    li.innerHTML = `
+        <input type="checkbox" class="checkbox">
+        <span>${taskText}</span>
+        <button class="delete-btn">X</button>
+    `;
 
-  const span = document.createElement("span");
-  span.textContent = taskText;
-  span.addEventListener("click", () => {
-    li.classList.toggle("completed");
-  });
+    taskList.appendChild(li);
+    input.value = "";
 
-  const removeBtn = document.createElement("button");
-  removeBtn.textContent = "❌";
-  removeBtn.addEventListener("click", () => {
-    taskList.removeChild(li);
-  });
+};
 
-  li.appendChild(span);
-  li.appendChild(removeBtn);
-  taskList.appendChild(li);
+function Tasks(e){
+    
+    // DELETE
+    if (e.target.classList.contains("delete-btn")) {
+        e.target.closest("li").remove();
+    }
 
-  task.value = "";
+    // CHECKBOX
+    if (e.target.classList.contains("checkbox")) {
+        const li = e.target.closest("li");
+
+        if (e.target.checked) {
+            li.style.textDecoration = "line-through";
+        } else {
+            li.style.textDecoration = "none";
+        }
+    }
+};
+
+
+
+function filters(e){
+    if (!e.target.id) return;
+
+    const tasks = document.querySelectorAll("#taskList li");
+
+    if (e.target.id === "all") {
+        tasks.forEach(li => li.style.display = "flex");
+    }
+
+    else if (e.target.id === "completed") {
+        tasks.forEach(li => {
+            const checkbox = li.querySelector(".checkbox");
+            li.style.display = checkbox.checked ? "flex" : "none";
+        });
+    }
+
+    else if (e.target.id === "pending") {
+        tasks.forEach(li => {
+            const checkbox = li.querySelector(".checkbox");
+            li.style.display = !checkbox.checked ? "flex" : "none";
+        });
+    }
+
+};
+
+
+addBtn.addEventListener('click', addTask);
+
+taskList.addEventListener("click", Tasks);
+
+filter.addEventListener('click', filters);
+
+input.addEventListener("keypress",(e)=>{
+    if(e.key==="Enter"){
+        addTask()
+    } 
 });
